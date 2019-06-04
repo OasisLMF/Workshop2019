08848791737f400afaa5762f35a334710da8444f
Mark Pinkerton <mark.pinkerton@oasislmf.org>
2019-06-04 13:08:54 +0100

    Adding

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 sudo cat apt.txt | xargs apt-get install -y
 
-virtualenv venv
+virtualenv -p /usr/bin/pyjon3.6 venv
 source venv/bin/activate
 pip install -r requirements.txt
-jupyter nbextension enable --py --sys-prefix qgrid
+sudo jupyter nbextension enable --py --sys-prefix qgrid
+source venv/bin/deactivate
 
 git clone https://github.com/OasisLMF/gem
 cd gem/oasis-test/model_data/GMO
