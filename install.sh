15c491b9b75c83d3b283c5a16b3cbc752102f967
Mark Pinkerton <mark.pinkerton@oasislmf.org>
2019-06-05 17:01:25 +0100

    Further work

--- install.sh
+++ install.sh
@@ -3,7 +3,5 @@ sudo cat apt.txt | xargs apt-get install -y
 virtualenv -p /usr/bin/pyjon3.6 venv
 source venv/bin/activate
 pip install -r requirements.txt
-sudo jupyter nbextension enable --py --sys-prefix qgrid
-source venv/bin/deactivate
 
-cat gem/model_data/GMO/footprint_data/* > gem/model_data/footprint.csv
+cat gem/model_data/GMO/footprint_data/* > gem/model_data/GMO/footprint.csv
