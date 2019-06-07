63df64bca98104069a2bc960753d54bd03153ef6
Sam Gamble <hexadessa@gmail.com>
2019-06-07 15:26:12 +0100

    Fix for proxy server

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -2,7 +2,7 @@
 
 PASSWORD=""
 mkdir $HOME/.jupyter
-echo '{"NotebookApp": {"password":"'$PASSWORD'", "token":""}}' > $HOME/.jupyter/jupyter_notebook_config.json
+echo '{"NotebookApp": {"password":"'$PASSWORD'", "token":"", "base_url": "/jupyter"}}' > $HOME/.jupyter/jupyter_notebook_config.json
 
 . venv/bin/activate
 jupyter notebook --port=8888 --allow-root --no-browser --ip=0.0.0.0
