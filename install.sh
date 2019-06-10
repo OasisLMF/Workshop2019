189449acf13c5e52d00bbc945e213df0918c4862
Mark Pinkerton <mark.pinkerton@oasislmf.org>
2019-06-10 17:48:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cat apt.txt | xargs sudo apt-get install -y
-
-virtualenv -p python3 venv
+# cat apt.txt | xargs sudo apt-get install -y
+virtualenv -p /usr/bin/python3.6 venv
 source venv/bin/activate
 pip install -r requirements.txt
-
+pip install ipykernel
+ipython kernel install --user --name=OasisWorkshop2018
 cat gem/model_data/GMO/footprint_data/* > gem/model_data/GMO/footprint.csv
