2652dc345ad892c0e706f790f8c434c79ce6e1c9
Mark Pinkerton <mark.pinkerton@oasislmf.org>
2019-06-15 07:37:48 +0100

    Added commands to bring up system

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ pip install -r requirements.txt
 pip install ipykernel
 ipython kernel install --user --name=OasisWorkshop2018
 cat gem/model_data/GMO/footprint_data/* > gem/model_data/GMO/footprint.csv
+jupyter notebook  --NotebookApp.token='' --NotebookApp.password='' --no-browser --port=8888 --ip=0.0.0.0 --NotebookApp.base_url=/jupyter --allow-root
+docker-compose -f ./gem/docker-compose.oasis_ui.yml -f ./gem/docker-compose.yml up -d
